3489b2fae80e6877662c3c4f496fc526142172c9
Gautam Biswas <gautam_2005@rediffmail.com>
2020-02-15 20:36:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
-def main():
-    return "Welcome!"
-if __name__ == "__main__":
-    app.run()
+def hello():
+    return "Saurav new BCCI president And I Love Dogs"
